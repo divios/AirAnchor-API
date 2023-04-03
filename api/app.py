cf7afabdaa38e24ecef7eb0cf8f08d46403f99cb
sergiobernalparrondo <serberpar@gmail.com>
2023-04-03 22:57:01 +0200

    implemented workload test

--- api/app.py
+++ api/app.py
@@ -1,9 +1,21 @@
-from fastapi import FastAPI
-from api.client import LocationKeyClient
+from client import LocationKeyClient
 
-app = FastAPI()
+import asyncio
+import random
+
 client = LocationKeyClient(url= "http://192.168.1.169:8085")
 
-@app.get("/")
-def home():
-    return client.location(data="testingData")
+async def my_async_function():    
+    print("Running request...")
+    
+    client.location(hex(random.randint(0, 256)))
+
+    
+
+async def my_infinite_loop():
+    while True:
+        wait_time = random.uniform(0, 10)
+        await asyncio.sleep(wait_time)
+        asyncio.create_task(my_async_function())
+
+asyncio.run(my_infinite_loop())
